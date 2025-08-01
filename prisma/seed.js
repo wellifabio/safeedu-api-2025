2cd8c565dc64c2e548b3710ad3f2b5f2b147d163
wellifabio <wellifabio@hotmail.com>
2025-08-01 17:13:51 -0300

    trabalhando em seed

---
+++ prisma/seed.js
@@ -0,0 +1,39 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const fs = require('fs');
+
+//Importando os dados de arquivo csv para variáveis
+const motds = require("./seed/motds.json");
+const escolas = require("./seed/escolas.json");
+const comentarios = require("./seed/comentarios.json");
+
+
+async function main() {
+    for (const motd of motds) {
+        await prisma.motd.create({
+            data: motd
+        });
+    }
+    for (const escola of escolas) {
+        await prisma.escola.create({
+            data: escola
+        });
+    }
+    for (const comentario of comentarios) {
+        await prisma.comentario.create({
+            data: comentario
+        });
+    }
+}
+
+//Executando a função main
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+        console.log('Seed complete');
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    });
