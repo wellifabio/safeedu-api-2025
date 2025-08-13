786d48548d27af5dd582a7021396412bb6dcdd56
wellifabio <wellifabio@hotmail.com>
2025-08-13 09:27:16 -0300

    ajustes de porta

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 
+const port = process.env.PORT || 3000
 const app = express();
 
 const swaggerDoc = require('./swagger.json');
@@ -14,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(process.env.PORT, () => {
-    console.log('API SafeEdu respondendo em http://localhost:' + process.env.PORT);
-    console.log('Acesse a documentação em http://localhost:' + process.env.PORT + "/api-docs");
+app.listen(port, () => {
+    console.log('API SafeEdu respondendo em http://localhost:' + port);
+    console.log('Acesse a documentação em http://localhost:' + port + "/api-docs");
 });
